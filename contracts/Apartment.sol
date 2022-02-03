f40b181a04819ea948b2b95bf2feff28224d6a04
Karanji <96533025+MrAlto@users.noreply.github.com>
2022-02-03 12:50:38 +0100

    fix : package and ERC721 import

--- contracts/Apartment.sol
+++ contracts/Apartment.sol
@@ -1,11 +1,11 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Apartment is ERC721Enumerable {
+contract Apartment is ERC721 {
   using Counters for Counters.Counter;
 
   Counters.Counter private _counter;
--- contracts/Building.sol
+++ contracts/Building.sol
@@ -1,14 +1,14 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "./interfaces/IApartment.sol";
 
-contract Building is ERC721Enumerable, Ownable {
+contract Building is ERC721, Ownable {
   using SafeMath for uint256;
 
   address public aptContract;
